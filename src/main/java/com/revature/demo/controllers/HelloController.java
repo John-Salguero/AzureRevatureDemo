c2a2f53791a379cdfe24683fff9ee6dfc7810e1e
Accusedbold#0777 <Accusedbold@hotmail.com>
2022-03-14 09:10:11 -0700

    Initial commit

---
+++ src/main/java/com/revature/demo/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.revature.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot App!";
+    }
+
+    @GetMapping("/froot")
+    public String froot() {
+        return "Living life Dulce Vida";
+    }
+}
